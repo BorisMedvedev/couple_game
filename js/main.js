de1d8a2e46cbcb666cad2705868b0deee074fe4d
Your Name <you@example.com>
2022-06-28 21:57:41 +0300

    fdfdf

--- js/main.js
+++ js/main.js
@@ -1,19 +1,54 @@
-"use strict";
+import Card from './card.js'
 
-function createCards(number) {
-  let card = document.createElement("article");
-  card.textContent = number;
-  card.classList.add('card');
-  document.querySelector('.game__container').append(card);
+
+function newGame(container, cardsCount) {
+  let cardsNumberArray = [],
+    cardsArray = [],
+    firstCard = null,
+    secondCard = null;
+  for (let i = 1; i <= cardsCount / 2; i++) {
+    cardsNumberArray.push(i);
+    cardsNumberArray.push(i);
+  }
+  cardsNumberArray = cardsNumberArray.sort(() => Math.random() - 0.5);
+  for (const cardNumber of cardsNumberArray) {
+    cardsArray.push(new Card(container, cardNumber, flip));
+  }
+
+  function flip(card) {
+    if (firstCard !== null && secondCard !== null) {
+      if (firstCard.number != secondCard.number) {
+        firstCard.open = false;
+        secondCard.open = false;
+        firstCard = null;
+        secondCard = null;
+      }
+    }
+    if (firstCard == null) {
+      firstCard = card;
+    } else {
+      if (secondCard == null) {
+        secondCard = card;
+      }
+    }
+
+    if (firstCard !== null && secondCard !== null) {
+      if (firstCard.number == secondCard.number) {
+        firstCard.success = true;
+        secondCard.success = true;
+        firstCard = null;
+        secondCard = null;
+      }
+    }
+    if (document.querySelectorAll('.card.success').length == cardsNumberArray.length) {
+      alert('ПОБЕДА !!!!!!!')
+      container.innerHTML = ''
+      cardsNumberArray = []
+      cardsArray = []
+      firstCard = null
+      secondCard = null
+      newGame(container, cardsCount);
+    }
+  }
 }
-
-createCards(1);
-createCards(2);
-createCards(3);
-createCards(4);
-createCards(4);
-createCards(4);
-createCards(4);
-createCards(4);
-createCards(4);
-createCards(4);
+  newGame(document.querySelector('.game__container'), 6);
